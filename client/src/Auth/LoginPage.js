c5c9c1d021190d273e56fd8fbc86820ed9933bc2
Bkroland19 <rkbukenya@cis.mak.ac.ug>
2022-10-04 22:29:11 +0300

    uis touched but not finished

--- client/src/Auth/LoginPage.js
+++ client/src/Auth/LoginPage.js
@@ -13,51 +13,73 @@ import {
 
 const LoginPage = ({ onInputChange, onSubmit, handleAuthType }) => {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Form onSubmit={onSubmit}>
-            <h2 style={{ textAlign: "center" }}>Login</h2>
-            <FormGroup>
-              <Label for="emailField">Email</Label>
-              <Input
-                type="email"
-                name="email"
-                id="emailField"
-                placeholder="example@domain.com"
-                // value={emailField}
-                onChange={onInputChange}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label for="passwordField">Password</Label>
-              <Input
-                type="password"
-                name="password"
-                id="passwordField"
-                placeholder="xxxxxxxx"
-                // value={passwordField}
-                onChange={onInputChange}
-              />
-            </FormGroup>
+		<Container
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+        // width:"100%",
+        padding:20,
+        backgroundColor:"#C6D8FF",
+        fontFamily:"fantasy"
+        // backgroundImage:"url('https://res.cloudinary.com/itgenius/image/upload/v1664902648/1_mwpuwHSsNIlIirvWdjgIbw_adeo16.jpg')"
+			}}
+		>
+			<Row style={{ width: "50%" }}>
+				<Col>
+					<Form onSubmit={onSubmit}>
+						<h3
+							style={{
+								textAlign: "center",
+								
+								fontFamily: "fantasy",
+							}}
+						>
+							<span style={{fontSize:14}}>Welcome To</span> <span style={{color:"blue " , fontFamily:"fantasy" }} >Digiti-Land</span>
+						</h3>
+						<FormGroup>
+							<Label for="emailField">Email</Label>
+							<Input
+								type="email"
+								name="email"
+								id="emailField"
+								placeholder="example@domain.com"
+								// value={emailField}
+								onChange={onInputChange}
+							/>
+						</FormGroup>
+						<FormGroup>
+							<Label for="passwordField">Password</Label>
+							<Input
+								type="password"
+								name="password"
+								id="passwordField"
+								placeholder="xxxxxxxx"
+								// value={passwordField}
+								onChange={onInputChange}
+							/>
+						</FormGroup>
 
-            <FormGroup check>
-              <Label check>
-                <Input type="checkbox" checked onChange={onInputChange} />{" "}
-                Remember me
-              </Label>
-            </FormGroup>
+						<FormGroup check>
+							<Label check>
+								<Input type="checkbox" checked onChange={onInputChange} />{" "}
+								Remember me
+							</Label>
+						</FormGroup>
 
-            <p>
-              <Link to="/auth/register">Create Account</Link>
-            </p>
+						<p >
+							<Link to="/auth/register">Create Account</Link>
+						</p>
 
-            <Button type="submit">Login</Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
-  );
+						<Button style={{backgroundColor:"blue"}} type="submit">Login</Button>
+					</Form>
+				</Col>
+			</Row>
+			{/* <Row style={{width:"50%"}}>
+        <img src="https://res.cloudinary.com/itgenius/image/upload/v1664902648/1_mwpuwHSsNIlIirvWdjgIbw_adeo16.jpg" alt=""   />
+      </Row> */}
+		</Container>
+	);
 };
 
 export default LoginPage;
--- client/src/Auth/SignupPage.js
+++ client/src/Auth/SignupPage.js
@@ -20,97 +20,117 @@ const SignupPage = ({
   accountAddressField
 }) => {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Form onSubmit={onSubmit}>
-            <h2 style={{ textAlign: "center" }}>Register</h2>
-            <FormGroup>
-              <Label for="emailField">Email</Label>
-              <Input
-                type="email"
-                name="email"
-                id="emailField"
-                placeholder="example@domain.com"
-                onChange={onInputChange}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label for="passwordField">Password</Label>
-              <Input
-                type="password"
-                name="password"
-                id="passwordField"
-                placeholder="xxxxxxxx"
-                onChange={onInputChange}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label for="confirmPasswordField">Confirm Password</Label>
-              <Input
-                type="password"
-                name="confirmPassword"
-                id="confirmPasswordField"
-                placeholder="xxxxxxxx"
-                onChange={onInputChange}
-              />
-            </FormGroup>
+		<Container
+			style={{
+				position: "absoliute",
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				// width:"100%",
+				padding: 20,
+				backgroundColor: "#C6D8FF",
+				fontFamily: "fantasy",
+				// backgroundImage:"url('https://res.cloudinary.com/itgenius/image/upload/v1664902648/1_mwpuwHSsNIlIirvWdjgIbw_adeo16.jpg')"
+			}}
+		>
+			<Row>
+				<Col>
+					<Form onSubmit={onSubmit}>
+						<h3 style={{ textAlign: "center" }}>
+							{" "}
+							<span style={{ fontSize: 14 }}>Register With</span>{" "}
+							<span style={{ color: "blue" }}>Digiti Land</span>{" "}
+						</h3>
+						<FormGroup>
+							<Label for="emailField" >
+								Email
+							</Label>
+							<Input
+								type="email"
+								name="email"
+								id="emailField"
+								placeholder="example@domain.com"
+								onChange={onInputChange}
+							/>
+						</FormGroup>
+						<FormGroup>
+							<Label for="passwordField">Password</Label>
+							<Input
+								type="password"
+								name="password"
+								id="passwordField"
+								placeholder="xxxxxxxx"
+								onChange={onInputChange}
+							/>
+						</FormGroup>
+						<FormGroup>
+							<Label for="confirmPasswordField">Confirm Password</Label>
+							<Input
+								type="password"
+								name="confirmPassword"
+								id="confirmPasswordField"
+								placeholder="xxxxxxxx"
+								onChange={onInputChange}
+							/>
+						</FormGroup>
 
-            <FormGroup>
-              <Label for="accountAddressField">Ethereum address:</Label>
-              <Input
-                type="text"
-                name="accountAddressField"
-                id="accountAddressField"
-                placeholder="0x12782738232345456567"
-                onChange={onInputChange}
-                value={accountAddressField}
-              />
-            </FormGroup>
+						<FormGroup>
+							<Label for="accountAddressField">Ethereum address:</Label>
+							<Input
+								type="text"
+								name="accountAddressField"
+								id="accountAddressField"
+								placeholder="0x12782738232345456567"
+								onChange={onInputChange}
+								value={accountAddressField}
+							/>
+						</FormGroup>
 
-            <FormGroup tag="fieldset" value={registerAs}>
-              <legend>Register as:</legend>
-              <FormGroup check>
-                <Label check>
-                  <Input
-                    type="radio"
-                    name="registerAs"
-                    value="CONSUMER"
-                    onChange={handleAuthType("registerAs")}
-                  />
-                  Consumer
-                </Label>
-              </FormGroup>
-              <FormGroup check>
-                <Label check>
-                  <Input
-                    type="radio"
-                    name="registerAs"
-                    value="ESCROW"
-                    onChange={handleAuthType("registerAs")}
-                  />
-                  Escrow
-                </Label>
-              </FormGroup>
-            </FormGroup>
+						<FormGroup tag="fieldset" value={registerAs}>
+							<legend>Register as:</legend>
+							<FormGroup check>
+								<Label check>
+									<Input
+										type="radio"
+										name="registerAs"
+										value="CONSUMER"
+										onChange={handleAuthType("registerAs")}
+									/>
+									Consumer
+								</Label>
+							</FormGroup>
+							<FormGroup check>
+								<Label check>
+									<Input
+										type="radio"
+										name="registerAs"
+										value="ESCROW"
+										onChange={handleAuthType("registerAs")}
+									/>
+									Escrow
+								</Label>
+							</FormGroup>
+						</FormGroup>
 
-            <FormGroup check>
-              <Label check>
-                <Input type="checkbox" checked onChange={onInputChange} />{" "}
-                Remember me
-              </Label>
-            </FormGroup>
+						<FormGroup check>
+							<Label check>
+								<Input type="checkbox" checked onChange={onInputChange} />{" "}
+								Remember me
+							</Label>
+						</FormGroup>
 
-            <p>
-              <Link to="/auth/login">Already registered? Login</Link>
-            </p>
+						<p>
+							<Link to="/auth/login">Already registered? Login</Link>
+						</p>
 
-            <Button type="submit">Signup</Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
-  );
+						<Button style={{ backgroundColor: "blue" }} type="submit">
+							Signup
+						</Button>
+					</Form>
+				</Col>
+			</Row>
+		</Container>
+	);
 };
 
 export default SignupPage;
